a3726432b6e7640d74fe1d7296aa0018d7c597db
Estela <estelaxmele@gmail.com>
2023-06-10 21:28:37 +0200

    adding average

--- calculator/main.c
+++ calculator/main.c
@@ -82,7 +82,7 @@ void average(void)
 
 int main(void) {
     char operation;
-    printf("\nWelcome to my calculator");
+    printf("\n~Welcome to my calculator~");
     printf("\n\'+\'- addition\n\'-\'- subtraction\n\'*\'- multiplication\n\'/\'- division\n\'%%\'- modulus\n\'s\'- square root\n\'^\'- power of a number\n\'a\'- calculate the average\n");
     printf("\nChoose a calculator operation: ");
     scanf("%c", &operation);
