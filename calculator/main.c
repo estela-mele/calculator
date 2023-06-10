4289c83c25ccac4a35e29953e8d963e3c9d2beac
Estela <estelaxmele@gmail.com>
2023-06-10 21:16:40 +0200

    menu

--- calculator/main.c
+++ calculator/main.c
@@ -1,14 +1,10 @@
-//
-//  main.c
-//  calculator
-//
-//  Created by Stela on 10.6.23.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    char operation;
+        printf("\nWelcome to my calculator");
+        printf("\n\'+\'- addition\n\'-\'- subtraction\n\'*\'- multiplication\n\'/\'- division\n\'%%\'- modulus\n\'s\'- square root\n\'^\'- power of a number\n");
+        printf("\nChoose a calculator operation: ");
+        scanf("%c", &operation);
     return 0;
 }
