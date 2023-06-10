87a771fa8310d90960a76be0f863d33133b08837
Estela <estelaxmele@gmail.com>
2023-06-10 21:15:14 +0200

    Initial Commit

---
+++ calculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  calculator
+//
+//  Created by Stela on 10.6.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
